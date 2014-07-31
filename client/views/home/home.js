d372c436fbc27b1b9a0fb2b7029ef9c402426bde
Joshua Ohlman <codewriterohs@gmail.com>
2014-07-30 19:15:05 -0500

    Add a simple, html only form

--- client/views/home/home.js
+++ client/views/home/home.js
@@ -12,6 +12,19 @@ Template.Home.helpers({
 	}
 });
 
+Template.Home.events({
+	'click .btn-iamjoe': function (e, tmpl) {
+		this.set('name', 'Joe Bloh');
+	}
+	, 'submit .form-simple': function (e, tmpl) {
+		e.preventDefault();
+		alert("I am: " + this.item.name + "\n" + JSON.stringify(this.item));
+	}
+	, 'change .form-simple input': function (e, tmpl) {
+		this.set("name", e.currentTarget.value);
+	}
+});
+
 Template.input.events({
 	'keyup input': function (e, tmpl) {
 		var name = e.currentTarget.name;
